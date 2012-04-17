febd271562448999cafa09211ff04c34086397ee
Sam Ritchie <sritchie09@gmail.com>
2012-04-17 14:19:05 -0700

    added objenesis.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.esotericsoftware.kryo/kryo "2.02"
+(defproject com.twitter/kryo "2.04"
   :description "Fast, efficient Java serialization"
   :java-source-path "src"
   :jvm-opts ["-Xmx768m" "-server"]
@@ -7,6 +7,7 @@
   :javac-source-path [["src"] ["test"]]
   :repositories {"conjars" "http://conjars.org/repo/"}
   :dependencies [[com.esotericsoftware.reflectasm/reflectasm "1.02"]
+                 [org.objenesis/objenesis "1.2"]
                  [com.googlecode/minlog "1.2"]]
   :dev-dependencies [[junit "4.8.2"]
                      [lein-javac "1.3.0"]
